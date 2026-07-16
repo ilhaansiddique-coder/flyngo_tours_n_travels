32b38236b98a1e7b58534e81034e94c754c35ff0
AbdullahIlhaan <iamilhaansiddique@gmail.com>
2026-07-16 23:24:30 +0600

    fix: remove compiled JS files from git, add to gitignore

--- backend/src/common/decorators/current-tenant.decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CurrentTenantId = void 0;
-var common_1 = require("@nestjs/common");
-exports.CurrentTenantId = (0, common_1.createParamDecorator)(function (_data, ctx) {
-    var _a;
-    var request = ctx.switchToHttp().getRequest();
-    return request.tenantId || ((_a = request.user) === null || _a === void 0 ? void 0 : _a.tenantId) || null;
-});
--- backend/src/common/decorators/public.decorator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Public = exports.PUBLIC_KEY = void 0;
-var common_1 = require("@nestjs/common");
-exports.PUBLIC_KEY = 'isPublic';
-var Public = function () { return (0, common_1.SetMetadata)(exports.PUBLIC_KEY, true); };
-exports.Public = Public;
